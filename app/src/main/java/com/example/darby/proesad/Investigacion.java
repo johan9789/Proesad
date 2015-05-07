aaddedab6bf720c26e79a2685ae3c92b4b152c1a
Johan Pineda <jmpv567@gmail.com>
2015-05-07 14:13:18 -0500

    ...

--- app/src/main/java/com/example/darby/proesad/Investigacion.java
+++ app/src/main/java/com/example/darby/proesad/Investigacion.java
@@ -8,33 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.app.ActionBar;
 
-/**
- * Created by darby on 19/04/2015.
- */
 public class Investigacion extends ActionBarActivity {
-
-
+    private View rootview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // layout
         setContentView(R.layout.activity_investigacion);
-
 
         // FLECHA ATRAS
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);  // esto es la flecha del menu principal pascual
     }
 
-
-
-    View rootview;
     @Nullable
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.activity_investigacion, container, false);
         return rootview;
     }
+
 }
