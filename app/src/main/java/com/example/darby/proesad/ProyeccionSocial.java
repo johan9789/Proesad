ad30617a357cd9fbbd6f8e5f8be7e5fd1dfce4d3
DARBY <Darbypascual2015@gmail.com>
2015-05-07 14:42:52 -0500

    ffff

--- app/src/main/java/com/example/darby/proesad/ProyeccionSocial.java
+++ app/src/main/java/com/example/darby/proesad/ProyeccionSocial.java
@@ -53,7 +53,7 @@ public class ProyeccionSocial extends ActionBarActivity implements ActionBar.Tab
         // colores instanciando a values color.xml  ========   pascual
         ColorDrawable colorDrawable = new ColorDrawable(R.color.verde);
         actionBar.setBackgroundDrawable(colorDrawable);
-        ColorDrawable colorDrawable1 = new ColorDrawable(R.color.verde);
+ //  ColorDrawable colorDrawable1 = new ColorDrawable(R.color.verde);
 
           // FLECHA ... luego al manifes para dar privilegios
                          // ActionBar actionBar = getSupportActionBar();
