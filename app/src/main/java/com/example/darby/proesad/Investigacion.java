47337bc36853f5aa9ce10e9b4290b6ded0b5729e
Johan Pineda <jmpv567@gmail.com>
2015-05-07 15:01:45 -0500

    menu ¿?

--- app/src/main/java/com/example/darby/proesad/Investigacion.java
+++ app/src/main/java/com/example/darby/proesad/Investigacion.java
@@ -7,15 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.app.ActionBar;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class Investigacion extends ActionBarActivity {
     private View rootview;
-    private Spinner spnRecursos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +17,6 @@ public class Investigacion extends ActionBarActivity {
         setContentView(R.layout.activity_investigacion);
 
         start();
-        verListaRecursos();
     }
 
     @Nullable
@@ -35,21 +28,7 @@ public class Investigacion extends ActionBarActivity {
     private void start(){
         // FLECHA ATRAS
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);  // esto es la flecha del menu principal pascual
-        spnRecursos = (Spinner)findViewById(R.id.spnRecursos);
-    }
-
-    private void verListaRecursos(){
-        String[] recursos = {"Formato APA", "Bases de datos científicas", "Bibliotecas electrónicas"};
-        spnRecursos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, recursos));
-        spnRecursos.setOnItemSelectedListener(new OnItemSelectedListener(){
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id){
-                Toast.makeText(adapterView.getContext(), (String)adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent){}
-        });
+        actionBar.setDisplayHomeAsUpEnabled(true);  // esto es la flecha del menu principal
     }
 
 }
