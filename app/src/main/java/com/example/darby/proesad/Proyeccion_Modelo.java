7d706816327efcf28130b3db4d4bf91ef00a2d41
DARBY <Darbypascual2015@gmail.com>
2015-05-20 11:26:48 -0500

    Proyeccion

--- app/src/main/java/com/example/darby/proesad/Proyeccion_Modelo.java
+++ app/src/main/java/com/example/darby/proesad/Proyeccion_Modelo.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * Created by darby on 29/04/2015.
@@ -13,9 +14,17 @@ public class Proyeccion_Modelo extends Fragment {
 
     View rootView;
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.proyeccion_modelo,container,false);
         return rootView;
+
+
+
     }
+
+
 }
+
